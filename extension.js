b85bbde8cd250e1ffe75417b16ec10efb3806133
Ciccioarmory <ciccioarmory@hotmail.it>
2024-10-26 18:26:54 +0200

    "Refactor extension.js to create a webview provider and update package.json to register the webview"

--- extension.js
+++ extension.js
@@ -1,36 +1,59 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+class FiveMAnimationViewProvider {
+    constructor(context) {
+        this._context = context;
+    }
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
+    // This method will be called when the view is activated
+    resolveWebviewView(webviewView) {
+        webviewView.webview.options = {
+            enableScripts: true,
+        };
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fivemanimationflagscalculator" is now active!');
+        // Set the HTML content for the webview
+        webviewView.webview.html = this.getWebviewContent();
+    }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('fivemanimationflagscalculator.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from FivemAnimationFlagsCalculator!');
-	});
-
-	context.subscriptions.push(disposable);
+    getWebviewContent() {
+		return `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>FiveM Animation</title>
+			<style>
+				body, html {
+					margin: 0;
+					padding: 0;
+					height: 100%;
+					overflow: hidden; /* Prevent scrollbars on the body */
+				}
+				iframe {
+					width: 100%;
+					height: 100vh; /* Set height to 100vh for full viewport height */
+					border: none;
+				}
+			</style>
+		</head>
+		<body>
+			<iframe src="https://vespura.com/fivem/animations/"></iframe>
+		</body>
+		</html>`;
+	}
 }
 
-// This method is called when your extension is deactivated
+function activate(context) {
+    const provider = new FiveMAnimationViewProvider(context);
+
+    context.subscriptions.push(
+        vscode.window.registerWebviewViewProvider('fivemAnimationView', provider)
+    );
+}
+
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
